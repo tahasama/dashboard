0414d241052324477aceb624c9ff17510cd212c2
tahasama <taha.maatof@gmail.com>
2025-01-24 14:24:07 +0100

    Fixed text color in md

--- app/(workflow)/LateAnalysisReviewConclusion.tsx
+++ app/(workflow)/LateAnalysisReviewConclusion.tsx
@@ -93,7 +93,7 @@ const LateAnalysisReviewConclusion: React.FC<{
         };
 
   return (
-    <div className="w-3/12 font-thin text-neutral-900 lg:text-slate-800 mb-2 pt-36 lg:mt-0 text-xs grid content-center scrollbar-thin  scrollbar-thumb-slate-600 scrollbar-track-slate-300 rounded-md scrollbar-corner-transparent overflow-y-scroll">
+    <div className="w-3/12 font-thin text-neutral-900 lg:text-slate-800 mb-2 pt-40 lg:mt-0 text-xs grid content-center scrollbar-thin  scrollbar-thumb-slate-600 scrollbar-track-slate-300 rounded-md scrollbar-corner-transparent overflow-y-scroll">
       {/* Impact Insights for Review */}
       <p className={`p-2 rounded-md mb-2 ${reviewImpactInsight.color}`}>
         {reviewImpactInsight.message}
