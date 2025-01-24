f9f6a912fb77c360e1363a34d258cd26bffb58d1
tahasama <taha.maatof@gmail.com>
2025-01-24 13:56:03 +0100

    Added rotation message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import RotationMessage from "./RotationMessage";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -27,7 +28,8 @@ export default function RootLayout({
       <body
       // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="hidden md:block">{children}</div>
+        <RotationMessage />
       </body>
     </html>
   );
